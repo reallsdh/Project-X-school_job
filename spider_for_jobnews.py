d7285e70968200fbb98e72fd7ee9a979122dd008
reallsdh <44797585+reallsdh@users.noreply.github.com>
2018-11-07 10:40:01 +0800

    Update spider_for_jobnews.py

--- spider_for_jobnews.py
+++ spider_for_jobnews.py
@@ -1,8 +1,8 @@
 # -*- coding: utf8 -*-
 
-"""sprider for school_job_information_V1.0
+"""sprider for school_job_information_V1.1
    爬的是当前日期之后的宣讲会信息
-   返回的字段[公司，宣讲会地点，举办学校，举办时间]
+   返回的字段[公司，宣讲会地点，举办学校，详细链接，举办时间]
    会在代码路径下生成以当前日期命名的结果文件
    涉及高校[nuaa/seu...]
 """
@@ -27,8 +27,11 @@ def gethtml(url):
     soup = BeautifulSoup(c, 'lxml')
     return soup
 
-def getinfo(url):
-    jobinfo =[]
+
+def getinfo(url,jobinfo):
+    # jobinfo =[]
+    # headers =[u'公司名\t',u'宣讲会地址\t',u'学校\t',u'详细链接\t',u'宣讲会时间\n']
+    # jobinfo.append(headers)
     totalpage = getPages(url)
     nowaday_time = time.strftime("%Y-%m-%d", time.localtime())
     status = 'on' #标志位
@@ -42,12 +45,13 @@ def getinfo(url):
             jobname = arr.select('li.span1')[0].text.strip('\n')
             jobschool = arr.select('li.span5')[0].text
             jobroom = arr.select('li.span4')[0].text
+            jobdetails = url.split('/teachin')[0] + arr.find_all('a')[0]['href'] #招聘详细信息地址
             jobtime = arr.select('li.span5')[1].text
             if jobtime.split('\t')[0] < nowaday_time: #只需要当前时间节点之后的宣讲会信息
                 status = 'off'
                 break
             else:
-                jobinfo.append([jobname+'\t',jobroom+'\t',jobschool+'\t',jobtime+'\n'])
+                jobinfo.append([jobname+'\t',jobroom+'\t',jobschool+'\t',jobdetails+'\t',jobtime+'\n'])
             write_to_csv(jobinfo)
             jobinfo =[]
         if status =='off':
@@ -57,23 +61,28 @@ def get_school_name(): #可以维护一个高校就业网的库
     school_list = ['nuaa','seu','njust','nju','njupt','hhu']
     return school_list
 
+
 def write_to_csv(content=[]):
     time_sub = time.strftime("%Y-%m-%d", time.localtime())
     with open(u'job_information_'+time_sub+'.txt','a',encoding='utf8') as w:
         for cc in content:
             for dd in cc:
-                w.writelines(dd)
+          w.writelines(dd)
+
 
 if __name__ == '__main__':
     school_name = get_school_name()
     nowaday_time = time.strftime("%Y-%m-%d", time.localtime())
     print ('Today is :',nowaday_time)
+    jobinfo =[]
+    headers =[u'公司名\t',u'宣讲会地址\t',u'学校\t',u'详细链接\t',u'宣讲会时间\n']
+    jobinfo.append(headers)
     for name in school_name:
         url = 'http://' + name + '.91job.gov.cn/teachin/index?page=1'
         print ('Now crawling：',url.split('/teachin')[0])
         try:
             getPages(url)
-            getinfo(url)
+            getinfo(url,jobinfo)
         except:
             print (name,'Connection Error，Please Click:',url)
 
